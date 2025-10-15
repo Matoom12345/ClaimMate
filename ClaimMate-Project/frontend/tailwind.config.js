61ef862bb2d144e1111f681553f439f962ba591a
Harit <harit.man@ku.th>
2025-10-16 00:47:40 +0700

    Tailwind Added

---
+++ ClaimMate-Project/frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",  // ให้ Tailwind scan โค้ดใน src
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
